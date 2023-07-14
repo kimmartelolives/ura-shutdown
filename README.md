f82e717586258dfd03abcf05a95ea13b96cef259
Kim Martel Olives <61133176+kimmartelolives@users.noreply.github.com>
2023-07-15 03:56:35 +0800

    Add files via upload
    Modified ShutdownTimer

---
+++ README.md
@@ -0,0 +1,22 @@
+# ShutdownTimer
+A mini-tool for shutting down computer
+
+Shutdown Timer is a small and easy-to-use tool to countdown timer for shutting down your computer.
+
+##Features
+
+* Countdown timer for shutdown, restart or sign out computer.
+* Hybrid-boot option for shutdown task.
+	
+	Hybrid Boot is a technology that is available from Windows 8. It provides a computer or device with a booting time of less than 30 to 70 percent than Windows 7 and earlier versions. 
+* Warn user before execute the task.
+* Vietnamese/English user interface.
+
+##Requirements
+* Windows 7 or later
+* .NET Framework 4.5 or later
+
+##Screenshots
+![alt tag](https://raw.githubusercontent.com/chidokun/ShutdownTimer/master/ShutdownTimer/Images/MainWindow.png)
+
+![alt tag](https://raw.githubusercontent.com/chidokun/ShutdownTimer/master/ShutdownTimer/Images/AboutWindow.png)
---
+++ ShutdownTimer/About.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShutdownTimer
+{
+    public partial class frmAbout : Form
+    {
+        public frmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            InitializeLanguage();
+        }
+
+        #region Language
+        public Language currentLanguage = Program.mainWindow.currentLanguage;
+        private void InitializeLanguage()
+        {
+            lblTitle.Text = currentLanguage.Items["About.Title"];
+            lblVersion.Text = currentLanguage.Items["About.Version"] + Application.ProductVersion+ " (151018)";
+            btnClose.Text = currentLanguage.Items["About.ButtonClose"];
+        }
+        #endregion
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
